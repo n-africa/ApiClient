6a316359c611fbb58642f07daa25873d7f4541a7
Naima Africa Francis <naima.africa@gmail.com>
2020-09-01 17:26:19 -0400

    Finished API

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
 
 namespace ApiClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Lyric
     {
-      Console.WriteLine("Welcome to C#");
+        public string Lyrics { get; set; }
     }
-  }
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var responseAsStream = await client.GetStreamAsync("https://api.lyrics.ovh/v1/mariah_carey/can't_take_that_away");
+
+            List<Lyric> theLyrics = await JsonSerializer.DeserializeAsync<List<Lyric>>(responseAsStream);
+
+            foreach (var theLyric in theLyrics)
+            {
+
+                Console.WriteLine($"{theLyric.Lyrics}");
+            }
+        }
+    }
 }
